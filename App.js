c9ffd70ae193be278fa9511cc9da6c58696e8b2d
Lars Thorup <lars@zealake.com>
2021-04-18 16:24:11 +0200

    ble: device scan

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type { Node } from 'react';
 import {
   SafeAreaView,
@@ -17,6 +17,11 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+
+import { BleManager } from 'react-native-ble-plx';
+
+// TODO: encapsulate
+const bleManager = new BleManager();
 
 const Section = ({ children, title }): Node => {
   return (
@@ -32,13 +37,39 @@ const Section = ({ children, title }): Node => {
 };
 
 const App: () => Node = () => {
+  console.log('App.render');
+  const [bleState, setBleState] = useState('PoweredOn'); // TODO: bleManager.onStateChange
+  const [deviceSet, setDeviceSet] = useState({});
+  useEffect(() => {
+    if (bleState !== 'PoweredOn') return;
+    console.log('App.deviceScanEffect.enter');
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log('bleManager.onDeviceScanError', error);
+        return;
+      }
+      if (device.name && !deviceSet[device.name]) {
+        console.log(device.name);
+        setDeviceSet(Object.assign({}, deviceSet, { [device.name]: true }));
+      }
+      // TODO: remove device
+    });
+    return () => {
+      console.log('App.deviceScanEffect.enter');
+      bleManager.stopDeviceScan();
+    }
+  });
   return (
     <SafeAreaView>
       <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
-          <Section title="BLE Sandbox">
-            ...todo: show device list
+          <Section title="BLE Sandbox" />
+          <Section title="BLE state">
+            {bleState}
+          </Section>
+          <Section title="BLE device list">
+            {Object.keys(deviceSet).join(', ')}
           </Section>
         </View>
       </ScrollView>
