be3a23ca3b63984bd04aeebbddacfdc78aa41ee4
Morten Ulrik Sorensen <mus@bestbrains.dk>
2021-05-10 21:33:46 +0200

    base64 string decoding

--- capture/src/lib/base64.js
+++ capture/src/lib/base64.js
@@ -2,3 +2,4 @@ import { Buffer } from 'buffer';
 
 export const uint8FromBase64 = (data) => Buffer.from(data, 'base64')[0];
 export const base64FromUint8 = (value) => Buffer.from([value]).toString('base64');
+export const stringFromBase64 = (data) => Buffer.from(data, 'base64').toString('ascii');
