286278495191d388a860259a0d6e62cab9fc3c58
Lars Thorup <lars@zealake.com>
2021-05-29 09:51:56 +0200

    fix: avoid jsx default export

--- capture/index.js
+++ capture/index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import { MochaRunnerScreen } from '@larsthorup/react-native-mocha';
+import MochaRunnerScreen from '@larsthorup/react-native-mocha/MochaRunnerScreen';
 import { name as appName } from './app.json';
 import mocha from './src/lib/mochaLoader';
 
--- packages/@larsthorup/react-native-mocha/index.js
+++ packages/@larsthorup/react-native-mocha/index.js
@@ -1,7 +1,6 @@
 import * as testRunnerJsonProtocol from './testRunnerJsonProtocol.js';
 import * as mochaEventReporter from './MochaEventReporter.js';
-import mrScreen from './MochaRunnerScreen.js';
 
 export const { parseTestRunnerEvent, stringifyTestRunnerEvent } = testRunnerJsonProtocol;
 export const { MochaEventReporter } = mochaEventReporter;
-export const MochaRunnerScreen = mrScreen;
+// Note: MochaRunnerScreen.js is deliberately not included as Node will not parse JSX
