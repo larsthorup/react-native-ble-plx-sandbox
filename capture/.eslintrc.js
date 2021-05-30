980c9668678d4da3916ff0a91c53cf63452318ae
Lars Thorup <lars@zealake.com>
2021-05-30 13:11:42 +0200

    rn  template: sample test

--- capture/.eslintrc.js
+++ capture/.eslintrc.js
@@ -5,15 +5,9 @@ module.exports = {
     'jest/valid-expect': 'off',
     'prettier/prettier': 'off',
   },
-  // Note: needed for top level await in testRunnerCli
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
   overrides: [
     {
-      files: '*.test.js',
+      files: './src/test/*.test.js',
       env: {
         'mocha': true,
       },
--- packages/@larsthorup/react-native-mocha-template/template/index.js
+++ packages/@larsthorup/react-native-mocha-template/template/index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import MochaRunnerScreen from '@larsthorup/react-native-mocha/MochaRunnerScreen';
+import { name as appName } from './app.json';
+import mocha from './src/lib/mochaLoader';
 
-AppRegistry.registerComponent(appName, () => App);
+// Note: importing all files in directory, supported by "babel-plugin-wildcard"
+import * as test from './src/test'; // eslint-disable-line no-unused-vars
+
+MochaRunnerScreen.mocha = mocha;
+AppRegistry.registerComponent(appName, () => MochaRunnerScreen);
